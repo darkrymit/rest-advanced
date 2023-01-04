2945db3ef2549bb46cb94c16c963a79643425cb7
Gurbanov Tamerlan <t64566t@gmail.com>
2023-01-04 15:26:06 +0200

    change(entity): move to jpa entity specification
    Rewrites entity's to jpa specification with code cleanup.

--- persistence/src/main/java/com/epam/esm/persistance/entity/GiftCertificate.java
+++ persistence/src/main/java/com/epam/esm/persistance/entity/GiftCertificate.java
@@ -1,37 +1,80 @@
 package com.epam.esm.persistance.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 /**
  * Data class represents gift certificate entity.
  *
  * @author Tamerlan Hurbanov
  * @see Entity
- * @since  1.0
+ * @since 1.0
  */
-@Data
+@Entity
+@Table(name = "certificates")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor
-public class GiftCertificate implements Entity {
+public class GiftCertificate {
 
   public static final String TABLE_NAME = "certificates";
 
   public static final String CERTIFICATES_TAGS_KEYS_TABLE = "certificates_has_tags";
 
+  @Id
+  @Column(unique = true, nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String description;
   private BigDecimal price;
   private Integer duration;
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+
+  @Column(name = "create_date")
   private ZonedDateTime createDate;
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+  @Column(name = "last_update_date")
   private ZonedDateTime lastUpdateDate;
+
+  @ManyToMany
+  @JoinTable(name = "certificates_has_tags",
+      joinColumns = {@JoinColumn(name = "certificates_id")},
+      inverseJoinColumns = {@JoinColumn(name = "tags_id")})
+  @ToString.Exclude
   private List<Tag> tags;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
+    }
+    GiftCertificate that = (GiftCertificate) o;
+    return id != null && Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }
--- persistence/src/main/java/com/epam/esm/persistance/entity/Tag.java
+++ persistence/src/main/java/com/epam/esm/persistance/entity/Tag.java
@@ -1,8 +1,18 @@
 package com.epam.esm.persistance.entity;
 
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 /**
  * Data class represents tag entity.
@@ -11,15 +21,22 @@ import lombok.NoArgsConstructor;
  * @see Entity
  * @since  1.0
  */
-@Data
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor
-public class Tag implements Entity {
+public class Tag {
 
   public static final String TABLE_NAME = "tags";
 
   public static final String CERTIFICATES_TAGS_KEYS_TABLE = GiftCertificate.CERTIFICATES_TAGS_KEYS_TABLE;
 
+  @Id
+  @Column(unique = true, nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
@@ -27,5 +44,22 @@ public class Tag implements Entity {
   public Tag(String name) {
     this.id = null;
     this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
+    }
+    Tag tag = (Tag) o;
+    return id != null && Objects.equals(id, tag.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
   }
 }
