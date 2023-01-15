8eb0532622d72619a385acf30f8ac99e4687edcb
Gurbanov Tamerlan <t64566t@gmail.com>
2023-01-15 21:13:23 +0200

    refactor(persistence): use SimpleJpaRepository to reduce duplication code

--- persistence/src/main/java/com/epam/esm/persistance/dao/impl/jpa/GiftCertificateJPARepository.java
+++ persistence/src/main/java/com/epam/esm/persistance/dao/impl/jpa/GiftCertificateJPARepository.java
@@ -5,14 +5,8 @@ import com.epam.esm.persistance.dao.GiftCertificateSearchParameters;
 import com.epam.esm.persistance.dao.TagRepository;
 import com.epam.esm.persistance.entity.GiftCertificate;
 import java.util.List;
-import java.util.Optional;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -24,60 +18,20 @@ import org.springframework.stereotype.Repository;
  * @since 1.0
  */
 @Repository
-@RequiredArgsConstructor
-@AllArgsConstructor
-public class GiftCertificateJPARepository implements GiftCertificateRepository {
+public class GiftCertificateJPARepository extends SimpleJpaRepository<GiftCertificate,Long> implements GiftCertificateRepository {
 
-  @PersistenceContext
-  protected EntityManager entityManager;
-
-  private <S extends GiftCertificate> boolean isNew(S entity) {
-    return entity.getId() == null || entity.getId() == 0L;
+  @Autowired
+  public GiftCertificateJPARepository() {
+    super(GiftCertificate.class);
   }
 
-  private Class<GiftCertificate> getEntityType() {
-    return GiftCertificate.class;
+  public GiftCertificateJPARepository(EntityManager entityManager) {
+    super(GiftCertificate.class, entityManager);
   }
 
   @Override
-  public <S extends GiftCertificate> S save(S entity) {
-    if (isNew(entity)) {
-      entityManager.persist(entity);
-      return entity;
-    } else {
-      return entityManager.merge(entity);
-    }
-  }
-
-  @Override
-  public Optional<GiftCertificate> findById(Long id) {
-    return Optional.ofNullable(entityManager.find(getEntityType(), id));
-  }
-
-  @Override
-  public boolean existsById(Long id) {
-    return findById(id).isPresent();
-  }
-
-  @Override
-  public List<GiftCertificate> findAllAsList() {
-    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-    CriteriaQuery<GiftCertificate> query = builder.createQuery(getEntityType());
-    Root<GiftCertificate> root = query.from(getEntityType());
-    query.select(root);
-    return entityManager.createQuery(query).getResultList();
-  }
-
-  @Override
-  public void delete(GiftCertificate entity) {
-    if (isNew(entity)) {
-      return;
-    }
-    GiftCertificate existing = this.entityManager.find(getEntityType(), entity.getId());
-    if (existing != null) {
-      entityManager.remove(
-          this.entityManager.contains(entity) ? entity : this.entityManager.merge(entity));
-    }
+  protected <S extends GiftCertificate> Long getIdFromEntity(S entity) {
+    return entity.getId();
   }
 
   @Override
--- persistence/src/main/java/com/epam/esm/persistance/dao/impl/jpa/TagJPARepository.java
+++ persistence/src/main/java/com/epam/esm/persistance/dao/impl/jpa/TagJPARepository.java
@@ -2,15 +2,9 @@ package com.epam.esm.persistance.dao.impl.jpa;
 
 import com.epam.esm.persistance.dao.TagRepository;
 import com.epam.esm.persistance.entity.Tag;
-import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -22,68 +16,24 @@ import org.springframework.stereotype.Repository;
  * @since 1.0
  */
 @Repository
-@RequiredArgsConstructor
-@AllArgsConstructor
-public class TagJPARepository implements TagRepository {
+public class TagJPARepository extends SimpleJpaRepository<Tag,Long> implements TagRepository {
 
-  @PersistenceContext
-  protected EntityManager entityManager;
-
-  private <S extends Tag> boolean isNew(S entity) {
-    return entity.getId() == null || entity.getId() == 0L;
+  @Autowired
+  public TagJPARepository() {
+    super(Tag.class);
   }
 
-  private Class<Tag> getEntityType() {
-    return Tag.class;
+  public TagJPARepository(EntityManager entityManager) {
+    super(Tag.class, entityManager);
   }
 
   @Override
-  public <S extends Tag> S save(S entity) {
-    if (isNew(entity)) {
-      entityManager.persist(entity);
-      return entity;
-    } else {
-      return entityManager.merge(entity);
-    }
-  }
-
-  @Override
-  public Optional<Tag> findById(Long id) {
-    return Optional.ofNullable(entityManager.find(getEntityType(), id));
-  }
-
-  @Override
-  public boolean existsById(Long id) {
-    return findById(id).isPresent();
-  }
-
-  @Override
-  public List<Tag> findAllAsList() {
-    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-    CriteriaQuery<Tag> query = builder.createQuery(getEntityType());
-    Root<Tag> root = query.from(getEntityType());
-    query.select(root);
-    return entityManager.createQuery(query).getResultList();
-  }
-
-  @Override
-  public void delete(Tag entity) {
-    if (isNew(entity)) {
-      return;
-    }
-    Tag existing = this.entityManager.find(getEntityType(), entity.getId());
-    if (existing != null) {
-      entityManager.remove(
-          this.entityManager.contains(entity) ? entity : this.entityManager.merge(entity));
-    }
+  protected <S extends Tag> Long getIdFromEntity(S entity) {
+    return entity.getId();
   }
 
   @Override
   public Optional<Tag> findByName(String name) {
-    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-    CriteriaQuery<Tag> query = criteriaBuilder.createQuery(getEntityType());
-    Root<Tag> root = query.from(getEntityType());
-    query.select(root).where(criteriaBuilder.equal(root.get("name"), name));
-    return entityManager.createQuery(query).getResultStream().findFirst();
+    return findOne((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("name"), name));
   }
 }
