2dd29222dafd148eedcb5ec2fecffc0a6e93e7dd
Gurbanov Tamerlan <t64566t@gmail.com>
2023-02-26 15:09:41 +0200

    change!: config as oauth2 resource server
    Config to use Jwt signed tokens from oauth authorization server instead of http basic auth.
    
    BREAKING CHANGE: Http Basic is not supported now

--- web/src/main/java/com/epam/esm/web/config/SecurityConfig.java
+++ web/src/main/java/com/epam/esm/web/config/SecurityConfig.java
@@ -2,20 +2,12 @@ package com.epam.esm.web.config;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-import com.epam.esm.persistance.dao.UserRepository;
-import com.epam.esm.web.security.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,30 +15,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   @Bean
-  public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
-    var authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(userDetailsService);
-    return new ProviderManager(authProvider);
-  }
-
-  @Bean
-  UserDetailsService userDetailsService(UserRepository userRepository) {
-    return new UserDetailsServiceImpl(userRepository);
-  }
-
-  @Bean
-  PasswordEncoder passwordEncoder(){
-    return NoOpPasswordEncoder.getInstance();
-  }
-
-  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http //
-        .csrf(AbstractHttpConfigurer::disable) //
-        .authorizeRequests(auth -> auth.anyRequest().authenticated()) //
-        .sessionManagement(session -> session.sessionCreationPolicy(STATELESS)) //
-        .httpBasic(Customizer.withDefaults()) //
+    // @formatter:off
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeRequests(auth -> auth.anyRequest().authenticated())
+        .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+        .oauth2ResourceServer(
+            OAuth2ResourceServerConfigurer::jwt
+        )
         .build();
+    // @formatter:on
   }
 
 }
