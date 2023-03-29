01426688bb269f2ba55f749738991ec1161fe2fe
Gurbanov Tamerlan <t64566t@gmail.com>
2023-03-30 00:04:09 +0300

    fix: war deploy
    Extend SpringBootServletInitializer to enable deploy on standalone tomcat server

--- web/src/main/java/com/epam/esm/web/RestAdvancedApplication.java
+++ web/src/main/java/com/epam/esm/web/RestAdvancedApplication.java
@@ -4,10 +4,11 @@ import com.epam.esm.web.config.KeycloakWebClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.epam.esm")
 @EnableConfigurationProperties(KeycloakWebClientProperties.class)
-public class RestAdvancedApplication {
+public class RestAdvancedApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(RestAdvancedApplication.class, args);
