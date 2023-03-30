5dcbafe9db59d5c87b32a69d775b39e73eb7748f
Gurbanov Tamerlan <t64566t@gmail.com>
2023-03-30 20:32:29 +0300

    refactor: simplify auditing setup

--- web/src/main/java/com/epam/esm/web/config/AuditConfig.java
+++ web/src/main/java/com/epam/esm/web/config/AuditConfig.java
@@ -1,14 +1,20 @@
 package com.epam.esm.web.config;
 
-import com.epam.esm.web.security.EmailAuditingProvider;
+import java.security.Principal;
+import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 public class AuditConfig {
 
   @Bean
-  EmailAuditingProvider emailAuditingProvider(){
-    return new EmailAuditingProvider();
+  AuditorAware<String> idAuditingProvider() {
+    return () -> Optional.of(SecurityContextHolder.getContext())
+        .map(SecurityContext::getAuthentication).map(Principal::getName);
   }
+
 }
