4d906d33e636b1ef4b8c47430d27ce9d43249a80
Gurbanov Tamerlan <t64566t@gmail.com>
2023-04-01 19:29:50 +0300

    test: add test to create method

--- service/src/test/java/com/epam/esm/service/impl/OrderServiceImplTest.java
+++ service/src/test/java/com/epam/esm/service/impl/OrderServiceImplTest.java
@@ -1,16 +1,22 @@
 package com.epam.esm.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.AdditionalAnswers.answer;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import com.epam.esm.persistance.dao.GiftCertificateRepository;
 import com.epam.esm.persistance.dao.OrderRepository;
+import com.epam.esm.persistance.dao.UserRepository;
 import com.epam.esm.persistance.entity.GiftCertificate;
 import com.epam.esm.persistance.entity.Order;
 import com.epam.esm.persistance.entity.OrderItem;
 import com.epam.esm.persistance.entity.Tag;
 import com.epam.esm.persistance.entity.User;
 import com.epam.esm.service.exceptions.NoSuchOrderException;
+import com.epam.esm.service.payload.request.OrderCreateRequest;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
@@ -22,6 +28,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer1;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceImplTest {
@@ -29,8 +36,21 @@ class OrderServiceImplTest {
   @Mock
   OrderRepository orderRepository;
 
+  @Mock
+  UserRepository userRepository;
+
+  @Mock
+  GiftCertificateRepository giftCertificateRepository;
+
   @InjectMocks
   OrderServiceImpl orderService;
+
+  private static Answer1<Order, Order> getFakeSave(Long id) {
+    return order -> {
+      order.setId(id);
+      return order;
+    };
+  }
 
   private User getUser() {
     return new User(UUID.fromString("028fcacb-b19b-4268-9e0c-6d96669b0d5e"), Instant.now());
@@ -73,5 +93,18 @@ class OrderServiceImplTest {
     when(orderRepository.findById(id)).thenReturn(Optional.empty());
 
     assertThrows(NoSuchOrderException.class, () -> orderService.getById(id,uuid));
+  }
+
+  @Test
+  void createShouldReturnOrderWithOwnerMatchingToUserIdtWhenNormalRequest() {
+    OrderCreateRequest request = new OrderCreateRequest(List.of("second"));
+    User owner = getUser();
+    GiftCertificate giftCertificate = getGiftCertificate();
+
+    when(userRepository.findById(owner.getId())).thenReturn(Optional.of(owner));
+    when(giftCertificateRepository.findAllByNames(request.getGiftCertificates())).thenReturn(List.of(giftCertificate));
+    when(orderRepository.save(any())).thenAnswer(answer(getFakeSave(1L)));
+
+    assertEquals(orderService.create(request,owner.getId()).getOwner(),owner);
   }
 }
